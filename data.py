cefd790ac6fb4394a4af552fb8d1e99e3dbf82bd
phuchb <habuiphuc@gmail.com>
2025-05-25 11:28:01 +0700

    a

--- data.py
+++ data.py
@@ -65,12 +65,13 @@ class GenerateDataset(Dataset):
         self.data = []
         with open(path_to_data, 'r') as f:
             for data in f:
+                print(data)
                 if 'xorqa' in path_to_data:
                     docid, passage, title = data.split('\t')
                     for lang in self.lang2mT5.values():
                         self.data.append((docid, f'Generate a {lang} question for this passage: {title} {passage}'))
                 else:
-                    docid, passage = data.split('+||||||+')
+                    docid, passage = data.split('\t')
                     self.data.append((docid, f'{passage}'))
 
         self.max_length = max_length
